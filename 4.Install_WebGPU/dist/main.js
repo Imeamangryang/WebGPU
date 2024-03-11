d26ed9c2757b6d89d823d6141b490230650d46f2
신준섭 <imeamangryang@naver.com>
2024-03-12 03:05:40 +0900

    first commit

---
+++ 4.Install_WebGPU/dist/main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=document.getElementById("compatibility-check");navigator.gpu?e.innerText="This browser can support WebGPU":e.innerText="This browser can not support WebGPU"})();
---
+++ 4.Install_WebGPU/src/main.ts
@@ -0,0 +1,8 @@
+const output_label : HTMLElement = <HTMLElement> document.getElementById("compatibility-check")
+
+if(navigator.gpu) {
+    output_label.innerText = "This browser can support WebGPU";
+}
+else {
+    output_label.innerText = "This browser can not support WebGPU";
+}
